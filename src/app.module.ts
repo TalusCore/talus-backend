79b3da9e2ad36c02f9630dccca54e3ba001b22ff
Nelson Loop <nelsonploop@gmail.com>
2025-06-10 16:27:41 -0400

    Implemented MQTT.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MqttModule } from './mqtt/mqtt.module';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MQTT_URL: Joi.string().uri().required(),
+        MQTT_PORT: Joi.number().required(),
+        MQTT_USERNAME: Joi.string().required(),
+        MQTT_PASSWORD: Joi.string().required()
+      })
+    }),
+    MqttModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
