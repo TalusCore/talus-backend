f074da523631ddee8b73e29cf3262aac0da84ca6
Nelson Loop <nelsonploop@gmail.com>
2025-06-23 16:12:59 -0400

    Fix Joi import and validation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MqttModule } from './mqtt/mqtt.module';
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        MQTT_URL: Joi.string().uri().required(),
+        MQTT_URL: Joi.string().required(),
         MQTT_PORT: Joi.number().required(),
         MQTT_USERNAME: Joi.string().required(),
         MQTT_PASSWORD: Joi.string().required()
