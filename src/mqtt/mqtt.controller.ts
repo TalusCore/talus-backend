90ba1a7851049b1e22d975c0feb4024bf290b85b
Nelson Loop <nelsonploop@gmail.com>
2025-07-03 17:15:57 -0400

    Only try and save stats to db if talus id exists.

--- src/mqtt/mqtt.controller.ts
+++ src/mqtt/mqtt.controller.ts
@@ -2,16 +2,32 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { LogUtil } from '../utils/log.util';
 import { StatService } from 'src/api/stat/stat.service';
+import { TalusService } from 'src/api/talus/talus.service';
 
 @Controller()
 export class MqttController {
-  constructor(private readonly statService: StatService) {}
+  constructor(
+    private readonly statService: StatService,
+    private readonly talusService: TalusService
+  ) {}
 
   @MessagePattern('sensor/data')
   async handleSensorData(@Payload() message: any) {
     try {
       const jsonMsg = JSON.parse(message);
       LogUtil.info('Received MQTT message:', jsonMsg);
+
+      if (!jsonMsg.talusId || !jsonMsg.stats || !jsonMsg.timestamp) {
+        LogUtil.warn('Invalid message format:', jsonMsg);
+        return;
+      }
+
+      const talus = await this.talusService.getTalusById(jsonMsg.talusId);
+
+      if (!talus) {
+        LogUtil.warn(`Talus with ID ${jsonMsg.talusId} not found.`);
+        return;
+      }
 
       const stats = await this.statService.createStats({
         talusId: jsonMsg.talusId,
