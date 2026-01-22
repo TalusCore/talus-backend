0f26d24aba729d4d8a70b9e39ab3599be9db5b2b
Nelson Loop <97408852+NelsonL2903@users.noreply.github.com>
2026-01-22 13:00:27 -0500

    Created sql migrations for db. (#14)
    * Created sql migrations for db.
    
    * Created stat indexes.

---
+++ supabase/migrations/20260121195339_create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
---
+++ supabase/migrations/20260121204522_create_user_talus_index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX idx_user_talus_user_id ON user_talus_relation (user_id);
