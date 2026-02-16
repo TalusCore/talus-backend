db91cb3d6bb6ee76b62063e3c7653d78b0571d43
Nelson Loop <97408852+NelsonL2903@users.noreply.github.com>
2026-02-16 18:16:11 -0500

    Added birthday, height, weight and gender (#18)

--- supabase/migrations/20260121195339_create_users_table.sql
+++ supabase/migrations/20260121195339_create_users_table.sql
@@ -1,4 +1,6 @@
-CREATE TABLE users (
+CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE TABLE public.users (
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
--- supabase/migrations/20260121204054_create_stats_table.sql
+++ supabase/migrations/20260121204054_create_stats_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE stats (
+CREATE TABLE public.stats (
     stat_name TEXT NOT NULL,
     talus_id UUID NOT NULL,
     timestamp TIMESTAMPTZ NOT NULL,
@@ -6,6 +6,6 @@ CREATE TABLE stats (
     PRIMARY KEY (stat_name, talus_id, timestamp),
     CONSTRAINT fk_talus
         FOREIGN KEY (talus_id)
-        REFERENCES talus(talus_id)
+    REFERENCES public.talus(talus_id)
         ON DELETE CASCADE
 );
