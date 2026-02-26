74e22bfee4d502933615f63c6ed7ed5edbe45b96
Nelson Loop <nelsonploop@gmail.com>
2026-02-26 16:12:06 -0500

    Made new ml insights api to make request to the new ml server.

--- src/api/stat/stat.controller.ts
+++ src/api/stat/stat.controller.ts
@@ -6,6 +6,7 @@ import { TalusService } from '../talus/talus.service';
 import { GetStatsDto } from './dto/get-stats.dto';
 import { FetchedStatDto } from './dto/fetched-stat.dto';
 import { GetStatsByNameRangeDto } from './dto/get-stats-by-name-range.dto';
+import { GetMLParamsDto } from './dto/get-ml-params.dto';
 
 @ApiTags('stat')
 @Controller('stat')
@@ -133,5 +134,48 @@ export class StatController {
       value: stat.value,
       timestamp: stat.timestamp
     }));
+  }
+
+  @Get('ml_insights')
+  @ApiOperation({
+    summary: 'Get ML insights for a Talus'
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'ML insights fetched successfully',
+    type: String
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Talus not found'
+  })
+  async fetchMLInsights(@Query() data: GetMLParamsDto): Promise<string> {
+    const existingTalus = await this.talusService.getTalusById(data.talusId);
+
+    if (!existingTalus) {
+      LogUtil.error(
+        `Talus with ID ${data.talusId} not found. Cannot fetch ML insights.`
+      );
+      throw new NotFoundException(
+        `Talus with ID ${data.talusId} not found. Cannot fetch ML insights.`
+      );
+    }
+
+    const mlParams = await this.statService.getMLParams(data);
+
+    const mlInsights = await this.statService.getMLInsights(
+      mlParams.age,
+      mlParams.gender,
+      mlParams.weight_kg,
+      mlParams.height_cm,
+      mlParams.steps_per_day,
+      mlParams.workout_frequency_per_week,
+      mlParams.avg_heart_rate,
+      mlParams.exercise_minutes_per_day,
+      mlParams.fitness_level
+    );
+
+    LogUtil.info(`Fetched ML insights for Talus with ID ${data.talusId}.`);
+    return mlInsights;
   }
 }
--- src/api/stat/stat.module.ts
+++ src/api/stat/stat.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { Stat } from 'src/entities/stat.entity';
 import { StatService } from './stat.service';
 import { StatController } from './stat.controller';
 import { TalusModule } from '../talus/talus.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Stat]), TalusModule],
+  imports: [TypeOrmModule.forFeature([Stat]), TalusModule, HttpModule],
   controllers: [StatController],
   providers: [StatService],
   exports: [StatService]
